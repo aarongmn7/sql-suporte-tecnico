26ac876d41ff39d57fe71c0d0063ee92f140544f
aaron gabriel maciel nascimento <aarongmn7@gmail.com>
2026-02-05 23:16:11 -0400

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuarios VALUES
+(1, 'Carlos Silva', 'carlos@empresa.com', 'Financeiro'),
+(2, 'Ana Souza', 'ana@empresa.com', 'Operações');
+
+INSERT INTO chamados VALUES
+(1, 1, 'Sistema', 'Erro ao acessar o sistema', 'Aberto', '2025-02-01', NULL),
+(2, 2, 'Hardware', 'Computador não liga', 'Fechado', '2025-01-28', '2025-01-29');
+
+INSERT INTO atendimentos VALUES
+(1, 1, 'Aaron', 'Reset de senha realizado', '2025-02-01'),
+(2, 2, 'Aaron', 'Fonte substituída', '2025-01-29');
